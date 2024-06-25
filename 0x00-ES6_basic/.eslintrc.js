f2ec9194acad804e4a1af4fb20a4a3980764f772
datlearner <kundaimuchemwa456@gmail.com>
2024-06-25 13:01:43 +0200

    fixing bugs

---
+++ 0x00-ES6_basic/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    // Add your custom rules here
+  },
+};
+
