b913698a9da19f85f102b8787a062f5588b31d68
datlearner <kundaimuchemwa456@gmail.com>
2024-06-25 15:32:30 +0200

    fixed arrow function

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,7 +1,6 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  // No need for 'self' with arrow functions (implicit 'this')
   this.addNeighborhood = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
     return this.sanFranciscoNeighborhoods;
