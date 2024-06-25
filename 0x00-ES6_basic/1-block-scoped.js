7878de5a6291156509eb84b5a40f0dd4e9f4490d
datlearner <kundaimuchemwa456@gmail.com>
2024-06-25 03:38:53 +0200

    modify variables inside a function

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,17 +1,13 @@
 export default function taskBlock(trueOrFalse) {
     const task = false;
     const task2 = true;
-  
+
     if (trueOrFalse) {
-      const task = true;
-      const task2 = false;
-      if (task && task2) {
-        return [task, task2];
-      }
+       const task = true;
+       const task2 = false;
     }
-  
+
     return [task, task2];
 }
-
 
 
