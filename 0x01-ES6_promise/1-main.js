d5a435a9ab065a0388f0f3b44d9d8d49bcd538f7
datlearner <kundaimuchemwa456@gmail.com>
2024-06-26 17:30:47 +0200

    Using the prototype below, return a promise. The parameter is a boolean.

--- 0x01-ES6_promise/1-main.js
+++ 0x01-ES6_promise/1-main.js
@@ -2,3 +2,11 @@ import getFullResponseFromAPI from './1-promise';
 
 console.log(getFullResponseFromAPI(true));
 console.log(getFullResponseFromAPI(false));
+
+getFullResponseFromAPI(true)
+     .then(response => console.log(response))
+     .catch(error => console.error(error));
+
+getFullResponseFromAPI(false)
+     .then(response => console.log(response))
+     .catch(error => console.error(error));
