93ba2394f9174767266c3d32f93a79d28086a638
datlearner <kundaimuchemwa456@gmail.com>
2024-06-26 17:22:54 +0200

    Using the prototype below, return a promise. The parameter is a boolean.

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,4 +1,10 @@
-export default function getFullResponseFromAPI(success) {
-    return success ? Promise.resolve({ status: 200, body: 'Success' }) : Promise.reject(Error('The fake API is not working currently'));
+export default function getFullResponseFromAPI(success){
+    return new Promise ((resolve, reject) => {
+        if (success) {
+            resolve({status: 200, body: 'Success'})
+        }
+        else{
+            reject(Error('The fake API is not working currently'))
+        }
+    })
 }
-
