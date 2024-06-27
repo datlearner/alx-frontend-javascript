1621bcd35a7a40a6c2748fb9145b3f1b7fb25a84
datlearner <kundaimuchemwa456@gmail.com>
2024-06-27 14:03:16 +0200

    fixed syntax error

--- 0x02-ES6_classes/10-main.js
+++ 0x02-ES6_classes/10-main.js
@@ -1,8 +1,8 @@
-import Car from "./10-car";
+import Car from './10-car';
 
 class TestCar extends Car {}
 
-const tc1 = new TestCar("Nissan", "Turbo", "Pink");
+const tc1 = new TestCar('Nissan', 'Turbo', 'Pink');
 const tc2 = tc1.cloneCar();
 
 console.log(tc1);
