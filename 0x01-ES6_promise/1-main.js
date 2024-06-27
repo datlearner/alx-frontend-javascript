3544ad20048aea1ebd4715ef0dbcc52e257980e5
datlearner <kundaimuchemwa456@gmail.com>
2024-06-27 06:26:28 +0200

    fixed some issues

--- 0x01-ES6_promise/1-main.js
+++ 0x01-ES6_promise/1-main.js
@@ -4,9 +4,9 @@ console.log(getFullResponseFromAPI(true));
 console.log(getFullResponseFromAPI(false));
 
 getFullResponseFromAPI(true)
-     .then(response => console.log(response))
-     .catch(error => console.error(error));
+  .then((response) => console.log(response))
+  .catch((error) => console.error(error));
 
 getFullResponseFromAPI(false)
-     .then(response => console.log(response))
-     .catch(error => console.error(error));
+  .then((response) => console.log(response))
+  .catch((error) => console.error(error));
