95ad5964ee421acf5a0788e2dc47a12dd2fb386a
datlearner <kundaimuchemwa456@gmail.com>
2024-06-27 15:26:12 +0200

    fixed packages issues

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,19 +1,25 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
-    node: true,
-    jest: true,
+    es6: true,
+    jest: true
   },
   extends: [
     'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:jest/recommended',
+    'airbnb-base'
   ],
   parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
+    ecmaVersion: 2018,
+    sourceType: 'module'
   },
+  plugins: [
+    'import',
+    'jest'
+  ],
   rules: {
     // Add your custom rules here
-  },
+  }
 };
-
