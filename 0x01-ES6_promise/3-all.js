208eb7ebab47b800fb48a366464308e3be1a57b7
datlearner <kundaimuchemwa456@gmail.com>
2024-06-27 09:05:37 +0200

    fixed unexpected consosle statement

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,13 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-    return Promise.all([uploadPhoto(), createUser()])
-        .then((results) => {
-            const photo = results[0];
-            const user = results[1];
-            console.log(`${photo} ${user.firstName} ${user.lastName}`);
-        })
-        .catch(() => {
-            console.log('Signup system offline');
-        });
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const photo = results[0];
+      const user = results[1];
+      console.log(`${photo} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
