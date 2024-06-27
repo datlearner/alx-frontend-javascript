da051cb467b22ae9a996f64f8722a2763bb30690
datlearner <kundaimuchemwa456@gmail.com>
2024-06-27 07:19:48 +0200

    fixing lint issues

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-    return new Promise((resolve, reject) => {
-        if (promise) {
-            resolve({status: 200, body: success});
-        }
-
-        else{
-            reject({});
-        };
-        
-    })
+  return new Promise((resolve, reject) => {
+    if (promise) {
+      resolve({ status: 200, body: 'success' });
+    } else {
+      reject({});
+    }
+  });
 }
