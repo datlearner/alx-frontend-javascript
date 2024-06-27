92ca0726f56f42bec510bdebd4ecbb2e9502e40b
datlearner <kundaimuchemwa456@gmail.com>
2024-06-27 13:38:18 +0200

    fixed syntax issue

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,25 +1,25 @@
 export default class Building {
-    constructor(sqft) {
-      if (typeof sqft !== 'number') {
-        throw new TypeError('sqft must be a number');
-      }
-      this._sqft = sqft;
-  
-      if (this.constructor !== Building) {
-        if (typeof this.evacuationWarningMessage !== 'function') {
-          throw new Error('Class extending Building must override evacuationWarningMessage');
-        }
-      }
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
     }
-  
-    get sqft() {
-      return this._sqft;
-    }
-  
-    set sqft(sqft) {
-      if (typeof sqft !== 'number') {
-        throw new TypeError('sqft must be a number');
+    this._sqft = sqft;
+
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
       }
-      this._sqft = sqft;
     }
   }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = sqft;
+  }
+}
