a35bb9b378e42b4fc23803e647c75e730c04a976
datlearner <kundaimuchemwa456@gmail.com>
2024-06-27 09:04:15 +0200

    Append three handlers to the function:

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,13 @@
 import { uploadPhoto, createUser } from "./utils";
 
 export default function handleProfileSignup() {
-    return Promise
-    .then(() => {
-        uploadPhoto()
-    }).then(() => {
-        createUser()
-    }).catch(() => {
-        new Error('Signup system offline')
-    })
+    return Promise.all([uploadPhoto(), createUser()])
+        .then((results) => {
+            const photo = results[0];
+            const user = results[1];
+            console.log(`${photo} ${user.firstName} ${user.lastName}`);
+        })
+        .catch(() => {
+            console.log('Signup system offline');
+        });
 }
