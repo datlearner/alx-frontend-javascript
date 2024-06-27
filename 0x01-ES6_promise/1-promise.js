bbeadc47977715c580b0a47214286e302b025a03
datlearner <kundaimuchemwa456@gmail.com>
2024-06-27 06:17:09 +0200

    Resolve merge conflict in 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,5 +1,4 @@
 export default function getFullResponseFromAPI(success) {
-<<<<<<< HEAD
   return new Promise((resolve, reject) => {
     if (success) {
       resolve({ staus: 200, body: 'Success' });
@@ -7,13 +6,5 @@ export default function getFullResponseFromAPI(success) {
       reject('The fake API is not working currently');
     }
   });
-=======
-    return new Promise((resolve, reject) => {
-        if (success) {
-            resolve({ status: 200, body: 'Success' });
-        } else {
-            reject(new Error('The fake API is not working currently'));
-        }
-    });
->>>>>>> 8efd212ba99c037b0f5e142163ada9d83495853b
 }
+
