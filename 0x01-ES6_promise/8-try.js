2d8fc853aa2887a7f4089db99c377cbb5015333a
datlearner <kundaimuchemwa456@gmail.com>
2024-06-27 11:09:10 +0200

    Write a function named divideFunction

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,12 +1,6 @@
 export default function divideFunction(numerator, denominator) {
-  try {
     if (denominator === 0) {
       throw new Error('cannot divide by 0');
-    } else {
-      numerator / denominator;
     }
-  } catch (error) {
-    console.log(error);
-  }
   return numerator / denominator;
 }
