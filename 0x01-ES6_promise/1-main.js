83e90bcf368f9c05e3c573ef99d45c288f35bc9a
datlearner <kundaimuchemwa456@gmail.com>
2024-06-27 06:27:23 +0200

    Save local changes to 1-main.js

--- 0x01-ES6_promise/1-main.js
+++ 0x01-ES6_promise/1-main.js
@@ -1,12 +1,5 @@
 import getFullResponseFromAPI from './1-promise';
 
-console.log(getFullResponseFromAPI(true));
-console.log(getFullResponseFromAPI(false));
+getFullResponseFromAPI(true);
+getFullResponseFromAPI(false);
 
-getFullResponseFromAPI(true)
-  .then((response) => console.log(response))
-  .catch((error) => console.error(error));
-
-getFullResponseFromAPI(false)
-  .then((response) => console.log(response))
-  .catch((error) => console.error(error));
