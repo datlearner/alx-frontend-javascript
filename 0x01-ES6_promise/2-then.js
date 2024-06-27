b9da930deb319f9444a2677456c8aba31989864d
datlearner <kundaimuchemwa456@gmail.com>
2024-06-27 06:42:15 +0200

    Append three handlers to the function:

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,10 @@
+export default function handleResponseFromAPI(promise) {
+    return new Promise((resolve, reject) => {
+        if (promise) {
+            resolve({status: 200, body: success});
+        }
+        else{
+            reject({})
+        }
+    })
+}
