d22faabfcfa9f90258c444a4ce6989348fe02a68
datlearner <kundaimuchemwa456@gmail.com>
2024-06-28 08:57:23 +0200

    Condense the internals of the following function to 1 line - without changing the name of each

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,3 +1,3 @@
-export default function getSumOfHoods(initialNumber, expansion1989 = undefined, expansion2019 = undefined) {
+export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
   return initialNumber + expansion1989 + expansion2019;
 }
