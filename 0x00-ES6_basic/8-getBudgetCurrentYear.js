a0c71f95641ff3eea854dd68a0a74733516365bc
datlearner <kundaimuchemwa456@gmail.com>
2024-06-28 10:46:03 +0200

    solved merging issue

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -5,9 +5,9 @@ function getCurrentYear() {
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
-    income: income,
-    gdp: income,
-    capita: capita,
+    income,
+    gdp,
+    capita,
   };
 
   budget[`income-${getCurrentYear()}`] = income;
