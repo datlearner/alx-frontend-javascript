a027afcf620f1a80b27f7a23b65f935023b9df7b
datlearner <kundaimuchemwa456@gmail.com>
2024-07-02 04:41:41 +0200

    Create a function getStudentsByLocation that returns an array of objects who are located in a specific city.

--- 0x03-ES6_data_manipulation/10-main.js
+++ 0x03-ES6_data_manipulation/10-main.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import updateUniqueItems from './10-update_uniq_items';
-import groceriesList from './9-groceries_list.js';
-=======
+
 import updateUniqueItems from "./10-update_uniq_items";
-import groceriesList from "./9-groceries_list";
->>>>>>> 5578f53570f062dd5cd51bdf88b07ad5c958a97e
 
 const map = groceriesList();
 console.log(map);
