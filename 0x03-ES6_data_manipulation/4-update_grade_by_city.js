d31ae4794752ae9b9a43cb6b4fae57d8b12fce40
datlearner <kundaimuchemwa456@gmail.com>
2024-07-02 04:57:20 +0200

    Create a function getStudentsByLocation that returns an array of objects who are located in a specific city.

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,13 +1,13 @@
-export default function updateStudentGradeByCity(studentObjects, city, newGrades) {
-  if (!Array.isArray(studentObjects) || !Array.isArray(newGrades)) {
-    return [];
-  }
 
-  return studentObjects.filter((student) => student.location === city)
-    .map((student) => {
-      const studentCopy = { ...student };
-      const studentGradeObj = newGrades.find((gradeObj) => gradeObj.studentId === studentCopy.id);
-      studentCopy.grade = studentGradeObj ? studentGradeObj.grade : 'N/A';
-      return studentCopy;
-    });
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students.filter((student) => student.location === city).map((student) => {
+    const newStudents = student;
+    newStudents.grade = 'N/A';
+    for (const grade of newGrades) {
+      if (newStudents.id === grade.studentId) {
+        newStudents.grade = grade.grade;
+      }
+    }
+    return newStudents;
+  });
 }
