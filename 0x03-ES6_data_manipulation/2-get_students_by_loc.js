c51c7a70239732445e233943807625299c064980
datlearner <kundaimuchemwa456@gmail.com>
2024-07-02 03:53:13 +0200

    fixed lint problems

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,7 @@
 export default function getStudentsByLocation(students, city) {
-    if (Array.isArray(students) === false) {
-      return [];
-    }
-  
-    return students.filter((student) => (student.location === city));
+  if (Array.isArray(students) === false) {
+    return [];
   }
+
+  return students.filter((student) => (student.location === city));
+}
