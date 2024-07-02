1f77520de77291bfa9bb4a9e9ee51b8446b8390f
datlearner <kundaimuchemwa456@gmail.com>
2024-07-02 02:55:19 +0200

    fixed lint problems

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,15 +1,15 @@
 export default function getListStudents() {
-    const details = [
-        {
-            id: 1, firstName: 'Guillaume', location: 'San Francisco'
-        },
-        {
-            id: 2, firstName: 'James', location: 'Colombia'
-        },
-        {
-            id: 5, firstName: 'Serena', location: 'San Francisco'
-        }
-    ]
+  const details = [
+    {
+      id: 1, firstName: 'Guillaume', location: 'San Francisco',
+    },
+    {
+      id: 2, firstName: 'James', location: 'Colombia',
+    },
+    {
+      id: 5, firstName: 'Serena', location: 'San Francisco',
+    },
+  ];
 
-    return details;
+  return details;
 }
