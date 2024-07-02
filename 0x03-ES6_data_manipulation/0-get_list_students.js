96ed32220e5900d7f5a983573156680c575cdfad
datlearner <kundaimuchemwa456@gmail.com>
2024-07-02 02:44:15 +0200

    Create a function named getListStudents that returns an array of objects.

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,15 @@
+export default function getListStudents(details) {
+    const details = [
+        {
+            id: 1, firstName: 'Guillaume', location: 'San Francisco'
+        },
+        {
+            id: 2, firstName: 'James', location: 'Colombia'
+        },
+        {
+            id: 5, firstName: 'Serena', location: 'San Francisco'
+        }
+    ]
+    
+    return details;
+}
