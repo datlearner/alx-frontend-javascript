641ca15b1c71acdbdaeaca236adebe9be4f3d279
datlearner <kundaimuchemwa456@gmail.com>
2024-07-02 04:04:08 +0200

    Create a function getStudentsByLocation that returns an array of objects who are located in a specific city.

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,12 @@
+export default function createInt8TypedArray(length, position, value) {
+    const buffer = new ArrayBuffer(length);
+  
+    const int8d = new Int8Array(buffer);
+  
+    if (position < 0 || position >= length) throw new Error('Position outside range');
+    // throw new RangeError();
+  
+    int8d[position] = value;
+  
+    return new DataView(buffer);
+  }
